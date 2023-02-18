d813fb07d7387af015637d53ef3a7eea49b18a8a
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-02-18 10:39:27 +0500

    initialize

---
+++ src/app/database/knexfile.ts
@@ -0,0 +1,70 @@
+import type { Knex } from 'knex';
+import path from 'path';
+import { pg } from '../../config/conf';
+
+const {
+  database,
+  host,
+  maxPool,
+  migrationsTable,
+  minPool,
+  password,
+  port,
+  user,
+} = pg;
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      host,
+      port: +port,
+      database,
+      user,
+      password,
+    },
+    pool: {
+      min: minPool,
+      max: maxPool,
+    },
+    migrations: {
+      tableName: "migrations",
+    },
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      host,
+      port: +port,
+      database,
+      user,
+      password,
+    },
+    pool: {
+      min: minPool,
+      max: maxPool,
+    },
+    migrations: {
+      tableName: "migrations",
+    },
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      host,
+      port: +port,
+      database,
+      user,
+      password,
+    },
+    pool: {
+      min: minPool,
+      max: maxPool,
+    },
+    migrations: {
+      tableName: "migrations",
+    },
+  },
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import http from 'http';
+import App from "./server";
+import router from "./router";
+
+const ExpressApp = new App(router)
+
+const server = http.createServer(ExpressApp.getServer);
+
+// _________LISTEN PORT___________
+const port = process.env.PORT || 5000
+
+server.listen(port, () => console.log("Listening port on " + port))
